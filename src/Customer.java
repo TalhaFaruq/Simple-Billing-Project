087fbaef9fd7c6dc688ad356636df02fb92293b0
Talha Farooq <hafiztalha1997@gmail.com>
2021-12-29 20:39:59 +0500

    Updated Project

--- src/Customer.java
+++ src/Customer.java
@@ -7,20 +7,20 @@ public class Customer implements ICustomer {
     public void getCustomerDetails() {
         Scanner input = new Scanner(System.in);
 
-//        System.out.println("Enter ID:");
-        customerDetails.setCustomerID(1L);
-//        System.out.println("Enter Name:");
-        customerDetails.setCustomerName("Talha");
-//        System.out.println("Enter Address");
-        customerDetails.setCustomerAddress("Islamabad");
-//        System.out.println("Enter Contact Number:");
-        customerDetails.setCustomerContact(02342342234);
-//        System.out.println("Enter Email");
-        customerDetails.setCustomerEmail("talha@gmail.com");
-//        System.out.println("Enter PanCard Number");
-        customerDetails.setPanCardNumber(2134125L);
-//        System.out.println("Enter Aadhar Card Number");
-        customerDetails.setAadharCard_Number(493753L);
+        System.out.println("Enter ID:");
+        customerDetails.setCustomerID(input.nextLong());
+        System.out.println("Enter Name:");
+        customerDetails.setCustomerName(input.next());
+        System.out.println("Enter Address");
+        customerDetails.setCustomerAddress(input.next());
+        System.out.println("Enter Contact Number:");
+        customerDetails.setCustomerContact(input.nextInt());
+        System.out.println("Enter Email");
+        customerDetails.setCustomerEmail(input.next());
+        System.out.println("Enter PanCard Number");
+        customerDetails.setPanCardNumber(input.nextLong());
+        System.out.println("Enter Aadhar Card Number");
+        customerDetails.setAadharCard_Number(input.nextLong());
     }
 
     @Override
